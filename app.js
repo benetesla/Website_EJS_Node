f3e6674d3d323d13389f60f8c45b0672009c8fb8
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-22 21:25:05 -0300

    Ejs template
    Ejs website

---
+++ app.js
@@ -0,0 +1,32 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sobre = require('./routes/sobre')
+const Home = require('./routes/home')
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', Home)
+app.use('/sobre' ,sobre);
+app.use(function(req, res, next) {
+    
+  next(createError(404));
+});
+
+app.use(function(err, req, res) {
+  res.locals.message = err.message; 
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
